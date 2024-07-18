b74857dea2ea64967f84b8e7758f6c2cffc81a76
Anatolii <kabantolyan@gmail.com>
2024-07-18 13:16:24 +0300

    Includes alternative solution

--- src/ts/Includes.ts
+++ src/ts/Includes.ts
@@ -5,3 +5,10 @@ type Includes<T extends readonly unknown[], U> = T extends readonly [infer First
   : false
 
 type isPillarMen = Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'> // expected to be `false`
+
+// Alternative solution
+
+// type Includes<T extends readonly unknown[], U> =
+//   T extends [infer First, ...infer Rest]
+//     ? Equal<First, U> extends true ? true : Includes<Rest, U>
+//     : false;
